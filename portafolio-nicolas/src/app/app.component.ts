de96bbaa2feb06d1b83941b724740caa0f4bcfb6
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-05-31 21:11:23 -0400

    feat: base layout

--- portafolio-nicolas/src/app/app.component.ts
+++ portafolio-nicolas/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink  ,RouterOutlet, RouterLinkActive} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+import {ProjectsComponent} from "./projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
+  imports: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portafolio-nicolas';
